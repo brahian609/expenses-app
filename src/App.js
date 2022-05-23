15098419066355a324bd9a3b8088b9ab8139ba14
Brahian Giraldo Medina <brahian609@hotmail.com>
2022-05-23 18:32:43 -0500

    Add new component for date format and style

--- src/App.js
+++ src/App.js
@@ -29,18 +29,18 @@ function App() {
       <ExpenseItem
         title={expenses[0].title}
         amount={expenses[0].amount}
-        date={expenses[0].date}>
-      </ExpenseItem>
+        date={expenses[0].date}
+      />
       <ExpenseItem
         title={expenses[1].title}
         amount={expenses[1].amount}
-        date={expenses[1].date}>
-      </ExpenseItem>
+        date={expenses[1].date}
+      />
       <ExpenseItem
         title={expenses[2].title}
         amount={expenses[2].amount}
-        date={expenses[2].date}>
-      </ExpenseItem>
+        date={expenses[2].date}
+      />
     </div>
   );
 }
