58d2608f9ee6b0bbd0b2606997d16a94a5b70f15
Brahian Giraldo Medina <brahian609@hotmail.com>
2022-05-27 14:47:48 -0500

    Add new component to render the expenses list

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
-import ExpenseItem from './ExpenseItem';
+import ExpensesList from './ExpensesList';
 import './Expenses.css';
 
 const Expenses = (props) => {
@@ -14,17 +14,12 @@ const Expenses = (props) => {
 
   return (
     <div>
-      <Card className="expenses">
-        <ExpensesFilter selected={filteredYear} onChangeFilter={onChangeFilterHandler} />
-        {filteredExpenses.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
-      </Card>
+      <li>
+        <Card className="expenses">
+          <ExpensesFilter selected={filteredYear} onChangeFilter={onChangeFilterHandler}/>
+          <ExpensesList expenses={filteredExpenses}/>
+        </Card>
+      </li>
     </div>
   );
 };
